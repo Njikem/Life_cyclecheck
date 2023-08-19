4243338c3bad0137cc0b94bfdc8fca477bb56d93
Njikem <123434988+Njikem@users.noreply.github.com>
2023-08-19 06:52:46 -0600

    first vue

---
+++ src/components/LifeCycleExample.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+     
+        
+
+        beforecreated: function (){
+            console.log('We just stated creating');
+        },
+
+        created: function(){
+            console.log(`created already`);
+        },
+
+        beforeMount: function() {
+            console.log('Soon to be mounted');
+            
+        },
+
+        mounted: function () {
+            console.log('Is mounted');
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
